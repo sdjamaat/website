f559c7197574c659db83d4e0a312cd4089eab783
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2020-05-14 04:51:09 -0700

    change ordering of imports

--- src/pages/committees.js
+++ src/pages/committees.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react"
-import { Table, Card, Divider } from "antd"
+import React from "react"
 import Layout from "../components/layout"
-import styled from "styled-components"
 import { committes } from "../../static/committee-info"
+import styled from "styled-components"
+import { Table, Card, Divider } from "antd"
 
 const CommitteeTable = ({ name, data }) => {
   const columns = [
