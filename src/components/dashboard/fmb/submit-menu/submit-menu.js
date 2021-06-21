6213c1cebc5f2b1050a413b802d52f258f67c477
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2021-06-20 19:16:09 -0700

    bug fix

--- src/components/dashboard/fmb/submit-menu/submit-menu.js
+++ src/components/dashboard/fmb/submit-menu/submit-menu.js
@@ -161,7 +161,7 @@ const SubmitFMBMenu = () => {
         title="Submit Thaali Choices"
         headStyle={{ fontSize: "1.5rem", textAlign: "center" }}
       >
-        {hasAlreadySubmitted && activeMenu !== null && activeMenu !== -1 && (
+        {hasAlreadySubmitted && (activeMenu !== null || activeMenu !== -1) && (
           <>
             <Alert
               style={{ marginBottom: "1rem" }}
