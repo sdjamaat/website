8e1d9896754e4e8ff2ad6af9e74ffd7569d5a8d6
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2021-12-29 17:42:36 -0800

    fix loader issue when submitting

--- src/components/dashboard/fmb/submit-menu/submit-menu.js
+++ src/components/dashboard/fmb/submit-menu/submit-menu.js
@@ -212,7 +212,7 @@ const SubmitFMBMenu = () => {
         title="Submit Thaali Choices"
         headStyle={{ fontSize: "1.5rem", textAlign: "center" }}
       >
-        {hasAlreadySubmitted && activeMenu !== null && activeMenu !== -1 && (
+        {hasAlreadySubmitted && activeMenu !== null && activeMenu !== -1 && !isSubmitting && (
           <>
             <Alert
               style={{ marginBottom: "1rem" }}
